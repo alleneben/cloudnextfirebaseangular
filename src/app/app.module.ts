1db6a258ff5be357ab9227b409bc541764bcc6e5
allen <alleneben@gmail.com>
2017-04-21 11:31:43 -0400

    Final CloudNext Firebase Tutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {AngularFireModule} from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxKCRRJ4bq-HU8-e6bZUqticvFSu5e3-Q",
+  authDomain: "cssfirebase.firebaseapp.com",
+  databaseURL: "https://cssfirebase.firebaseio.com",
+  projectId: "cssfirebase",
+  storageBucket: "cssfirebase.appspot.com",
+  messagingSenderId: "220671154675"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
